1c4db670b4ccc68c0fbda8efead7684628c4595c
Dileka <dileka.14@cse.mrt.ac.lk>
2021-07-13 20:36:38 +0530

    all working

--- LeetCode/46-try-2permutations.js
+++ LeetCode/46-try-2permutations.js
@@ -2,34 +2,34 @@
  * @param {number[]} nums
  * @return {number[][]} //123
  */
-var permute = function(nums) {
-    return swap(nums,0,[])
+var permute = function (nums) {
+    return convertToInt(swap(nums, 0, []))
 };
 
-const swap = (nums)=>{
-    console.log("start",nums)
-    if(nums.length===0){
+const swap = (nums) => {
+    console.log("start", nums)
+    if (nums.length === 0) {
         return nums;
     }
-    if(nums.length===1){
+    if (nums.length === 1) {
         return nums;
     }
 
     const first = nums[0].toString()
-    console.log("first",first)
+    console.log("first", first)
     const rest = swap(nums.slice(1))
-    console.log("rest",rest)
+    console.log("rest", rest)
     const solutions = []
-    for(let i=0;i<rest.length;i++){
+    for (let i = 0; i < rest.length; i++) {
         const temp = rest[i].toString()
-        console.log("temp",temp)
+        console.log("temp", temp)
         solutions.push(first.concat(temp))
-        for(let j=0;j<temp.length;j++){
-            const modified =temp[j].concat(first);
-            console.log("modified",modified)
-            const currentAns = temp.replace(temp[j],modified)
-            console.log("currentAns",currentAns)
-            solutions.push((currentAns))
+        for (let j = 0; j < temp.length; j++) {
+            const modified = temp[j].concat(first);
+            console.log("modified", modified)
+            const currentAns = temp.replace(temp[j], modified)
+            console.log("currentAns", currentAns)
+            solutions.push(currentAns)
 
         }
     }
@@ -37,18 +37,23 @@ const swap = (nums)=>{
 
 }
 
-const convertToInt=(string)=>{
-    const arr=[];
-    for(let i of string){
-        if(!isNaN(i)){
-            arr.push(parseInt(i))
-        }
+const convertToInt = (nums) => {
+    const answer = [];
+    for (let string of nums){
+        const arr = [];
+        for (let char of string) {
+            if (!isNaN(char)) {
+                arr.push(parseInt(char))
+            }
 
+        }
+        answer.push(arr);
     }
-    return arr.reverse();
+
+    return answer;
 }
 
-console.log(permute([1,2,3]))
+console.log(permute([1,2,3]));
 
 //[1,2,3]
 /// 1,2,3 1,3,2
