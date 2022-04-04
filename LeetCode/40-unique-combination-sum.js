070accd2f0fba55bb3a451978cddb2ee90ba2d27
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2022-04-04 18:13:47 +0530

    group by number to reduce running time

--- LeetCode/40-unique-combination-sum.js
+++ LeetCode/40-unique-combination-sum.js
@@ -5,11 +5,26 @@
  * Input: candidates = [2,3,5], target = 8
  * Output: [[2,2,2,2],[2,3,3],[3,5]]
  */
-var combinationSum = function (candidates, target) {
+var combinationSum2 = function (candidates, target) {
+    const sum = getSum(candidates);
+    if (sum<target) return []
+    let groups = candidates.reduce((r, a) => {
+        r[a] = [...r[a] || [], a];
+        return r;
+    }, {});
+    const groupCount = Object.keys(groups).length;
+    if(groupCount ===1) {
+        const quotient = target%candidates[0]
+        if(quotient!==0) return [];
+        const dividend = target/candidates[0]
+        return [...candidates.slice(0,dividend)]
+
+    };
     candidates.sort((a, b)=> a-b);
     const map = new Map();
+    const dynamicMap = new Map();
     const answers = [];
-    recursiveSeeker(0, candidates, target, [], map, 0)
+    recursiveSeeker(0, candidates, target, [], map, dynamicMap)
     for (const pair of map.entries()) {
         answers.push(pair[1])
     }
@@ -17,17 +32,24 @@ var combinationSum = function (candidates, target) {
 
 };
 
-const recursiveSeeker = (index, candidates, target, tempAns, answers, depth) => {
-    if (index > candidates.length || depth > target) return answers;
+const recursiveSeeker = (index, candidates, target, tempAns, answerMap, dynamicMap) => {
+    if(dynamicMap.has(`${tempAns.toString()}`)) return ;
+    if (index >= candidates.length) return ;
     const currentSum = getSum(tempAns);
-    if (currentSum > target) return answers;
+    if (currentSum > target) return answerMap;
     if (currentSum === target) {
-       answers.set(tempAns.toString(),tempAns);
+        dynamicMap.set(`${index}${tempAns.toString()}`,1);
+       answerMap.set(tempAns.toString(),tempAns);
        return;
     }
     const temp = candidates[index];
-    recursiveSeeker(index+1, candidates, target, [...tempAns, temp], answers, depth + 1);
-    recursiveSeeker(index+1, candidates, target, [...tempAns], answers, depth + 1);
+    if((currentSum + temp) === target){
+        dynamicMap.set(`${[...tempAns, temp].toString()}`,1);
+        answerMap.set([...tempAns, temp].toString(), [...tempAns, temp])
+        return ;
+    }
+    recursiveSeeker(index+1, candidates, target, [...tempAns, temp], answerMap, dynamicMap);
+    recursiveSeeker(index+1, candidates, target, [...tempAns], answerMap, dynamicMap);
 
 }
 
@@ -36,7 +58,8 @@ getSum = (arr) => {
     return arr.reduce((a, b) => a + b, 0);
 };
 
-console.log('Answers from function', (combinationSum([10,1,2,7,6,1,5], 8)));
+//console.log('Answers from function', (combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 30)));
+console.log('Answers from function', (combinationSum2([1], 1)));
 
 
 
