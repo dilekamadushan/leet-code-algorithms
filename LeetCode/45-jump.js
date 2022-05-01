9c8fd8570cd3236893c4ff1c5d7c9dad57d0cf3e
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2022-05-01 13:04:47 +0530

    include recursive calling

--- LeetCode/45-jump.js
+++ LeetCode/45-jump.js
@@ -5,7 +5,8 @@
  */
 var jump = function (nums) {
     // if only one element no need to jump
-    if (nums.length === 1) return 0;
+    if (!nums || nums.length === 1 || nums.length === 0) return 0;
+    if(nums[0]===0) return Infinity;
     let totalJumps = 0;
     let currentStep = 0;
     // if he can jump with the current step
@@ -17,11 +18,10 @@ var jump = function (nums) {
             currentStep++;
         } else {
             let nextIndex = getNextOptimalPoint(nums, currentStep, nums[currentStep])
-            if (nums[currentStep] > nums[nextIndex]) {
-                currentStep += nums[currentStep]
-            } else {
-                currentStep += nums[nextIndex];
-            }
+            const firstAns = totalJumps + 1 + jump(nums.slice(currentStep + nums[currentStep]));
+            const secondAns = totalJumps + 1 + jump(nums.slice(currentStep+nums[nextIndex]));
+            if (firstAns < secondAns) return firstAns;
+            return secondAns;
 
         }
 
@@ -52,11 +52,12 @@ var jump = function (nums) {
 };
 
 const getNextOptimalPoint = (arr, startIndex, step) => {
+    if (step === 0) return undefined;
     let optimalStep = 0;
     let tempMax = 0;
     arr.slice(startIndex + 1, startIndex + 1 + step)
         .forEach((element, index) => {
-            if (element > tempMax) {
+            if (element >= tempMax) {
                 optimalStep = index + startIndex + 1;
                 tempMax = element;
             }
@@ -64,4 +65,4 @@ const getNextOptimalPoint = (arr, startIndex, step) => {
     return optimalStep;
 }
 
-console.log(jump([4,1,1,3,1,1,1]))
+console.log(jump([5,9,3,2,1,0,2,3,3,1,0,0]))
